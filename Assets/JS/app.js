317607b1b1dcaa5fec79225211f9a6a471bed8e5
Racheal-spec <folorunshotosin334@gmail.com>
2020-09-19 18:21:45 +0100

    first commit

---
+++ Assets/JS/app.js
@@ -0,0 +1,6 @@
+const Burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.navlink');
+
+Burger.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+})
