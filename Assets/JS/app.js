74fd296227d794c5556365a8cddcd10bca126980
Racheal-spec <folorunshotosin334@gmail.com>
2020-09-28 16:07:10 +0100

    minified css and fixed navbar

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -1,6 +1,10 @@
-const Burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.navlink');
 
-Burger.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-});
+    const Burger = document.querySelector('.burger');
+    const navLinks = document.querySelector('.navlink');
+    
+    Burger.addEventListener('click', () => {
+        navLinks.classList.toggle('open');
+    });
+    navLinks.addEventListener('click', () => {
+        navLinks.classList.toggle('close');
+    })
